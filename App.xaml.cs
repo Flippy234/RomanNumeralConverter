8672515defbedd80269f4bde28ff2b5c7f95e971
Tyler Hanson <700211854@fvtc.edu>
2026-03-17 07:55:14 -0500

    Initial project push

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CoreText;
+using RomanConverter.Custom;
+
+namespace RomanConverter;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        RomanConverter = new RomanConverter();
+        MainPage = new AppShell();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace RomanConverter;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Roman Converter";
+    }
+
+
+    private void TxtRomanNumeral_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void btnClear_OnClicked(object sender, EventArgs e)
+    {
+        txtRomanNumeral.Text = string.Empty;
+    }
+
+    private void btnConvert_OnClicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
